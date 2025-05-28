28dd359aef36033a4bc5d6a26f5829e7fb8dffaa
uemerson199 <uemersonribeiro220@gmail.com>
2025-05-28 10:59:07 -0300

    Project created

---
+++ backend/src/main/java/com/ifsuldeminas/escrud/EscrudApplication.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.escrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ifsuldeminas/escrud/EscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.escrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
