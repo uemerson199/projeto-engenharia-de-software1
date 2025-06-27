6b7d465bcda4afe05502ba94b3430ee04248c890
uemerson199 <uemersonribeiro220@gmail.com>
2025-06-27 13:56:52 -0300

    feat: Inicia implementação da entidade Supplier

--- backend/src/main/java/com/ifsuldeminas/escrud/dto/SupplierDTO.java
+++ backend/src/main/java/com/ifsuldeminas/escrud/dto/SupplierDTO.java
@@ -1,4 +1,18 @@
 package com.ifsuldeminas.escrud.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class SupplierDTO {
+
+    private Long id;
+    private String name;
+    private String contactName;
+    private String phone;
+    private String email;
+    private String cnpj;
+
 }
--- backend/src/main/java/com/ifsuldeminas/escrud/repositories/SupplierRepository.java
+++ backend/src/main/java/com/ifsuldeminas/escrud/repositories/SupplierRepository.java
@@ -1,4 +1,10 @@
 package com.ifsuldeminas.escrud.repositories;
 
-public interface SupplierRepository {
+import com.ifsuldeminas.escrud.entities.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
 }
